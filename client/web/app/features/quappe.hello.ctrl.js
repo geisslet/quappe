a5cc86395842bdaeb3fb7ab7f95d706e69727b32
geisslet <tilo.geissler@gmail.com>
2016-04-06 22:42:41 +0200

    mainly some quappe.hello adoptions for style and conroller

--- client/web/app/features/quappe.hello.ctrl.js
+++ client/web/app/features/quappe.hello.ctrl.js
@@ -1,6 +1,21 @@
 angular.module('quappe')
     .controller('quappeHelloCtrl', quappeHelloCtrl);
 
-function quappeHelloCtrl(){
-        
-};
+quappeHelloCtrl.$inject = ['$scope','$log'];
+function quappeHelloCtrl($scope,$log){
+    'use strict';
+    
+    var vm = this;
+
+    vm.post = null;
+
+    vm.activate = function(){
+        $log.debug('quappeHelloCtrl.activate called');
+
+    };
+
+    vm.submit = function(){
+        $log.debug('quappeHelloCtrl.submit called - post: ' + vm.post);
+    };
+
+}
